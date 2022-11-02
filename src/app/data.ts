78a81de62f6c9596534a97e3f2db6293419e9279
Arista Indrajaya <arista.indrajaya@gmail.com>
2022-11-02 08:52:58 +0700

    data and data interface implementation

--- src/app/data.ts
+++ src/app/data.ts
@@ -1,9 +1,17 @@
 export interface Hero {
   name: string;
+  phone: string
 }
 
 export const HEROES = [
-  {name: 'Batman'},
-  {name: 'Spiderman'},
-  {name: 'Wonder Womant'}
+  {name: 'Batman', phone: "25565115"},
+  {name: 'Spiderman', phone: "25565115"},
+  {name: 'Wonder Woman', phone: "25565115"},
+  {name: 'Venom', phone: "25565115"}
 ]
+
+export interface Hero2 {
+  id: number;
+  name: string;
+  phone: number
+}
