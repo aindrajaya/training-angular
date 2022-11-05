c781a8a9b371110c03220d76616b67ad96ec50ce
Arista Indrajaya <arista.indrajaya@gmail.com>
2022-11-06 05:15:12 +0700

    first example

--- src/app/hello/hello.component.ts
+++ src/app/hello/hello.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Arr } from '../definition';
+import { Arr } from '../fundamental-interface/definition';
 
 @Component({
   selector: 'app-hello',
